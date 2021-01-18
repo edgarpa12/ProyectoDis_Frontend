7bec21db3c21afcf0f48864c94a5bde9c6298aca
Edgar <edgdav00@gmail.com>
2021-01-18 12:32:35 -0600

    heroku path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // uri: "https://proyecto-dis.herokuapp.com/api"
-  uri: 'http://localhost:3000/api'
+  // uri: 'http://localhost:3000/api'
+  uri: "https://proyecto-dis-backend.herokuapp.com/"
   // uri: 'http://192.168.192.20:3000/api'
 };
